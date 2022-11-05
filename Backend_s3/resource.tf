5591f290c46b4f80339031cc0c5ea82221dfc6af
vamsi <vamsikrishna.123.vk@gmail.com>
2022-11-05 15:32:17 +0530

    added the ec2 instance resource in the resource.tf file

--- Backend_s3/resource.tf
+++ Backend_s3/resource.tf
@@ -49,42 +49,61 @@ resource "aws_route_table_association" "ass_subnet" {
 // creating the security group
 
 resource "aws_security_group" "s3secgp" {
-   vpc_id = aws_vpc.s3vpc.id
-   description = "This is the security group for the s3 bucket"
-   ingress {
+  vpc_id      = aws_vpc.s3vpc.id
+  description = "This is the security group for the s3 bucket"
+  ingress {
     cidr_blocks = ["0.0.0.0/0"]
     description = "This is for ssh"
-    protocol = "tcp"
-    from_port = 22
-    to_port = 22
-   }
-   ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+  }
+  ingress {
     cidr_blocks = ["0.0.0.0/0"]
     description = "This is for http"
-    protocol = "tcp"
-    from_port = 80
-    to_port = 80
-   }  
-   ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+  }
+  ingress {
     cidr_blocks = ["0.0.0.0/0"]
     description = "This is for https"
-    protocol = "tcp"
-    from_port = 443
-    to_port = 443
-   }
-   egress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+  }
+  egress {
     cidr_blocks = ["0.0.0.0/0"]
     description = "To allow all"
-    protocol = "-1"
-    from_port = 0
-    to_port = 0
-   }  
-   tags = {
-     "Name" = "security_s3"
-   }
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
+  tags = {
+    "Name" = "security_s3"
+  }
+}
+// To create a ec2  instance first create a network interface 
+resource "aws_network_interface" "s3netinf" {
+  subnet_id = aws_subnet.s3subnets[0].id
+  tags = {
+    "Name" = "network_interface"
+  }
+
 }
 // creating the EC2 instance 
-
-resource "aws_instance" "name" {
-  
+resource "aws_instance" "s3instance" {
+  ami_id                 = "ami-062df10d14676e201" // EC2 ubuntu machine in mumbai region
+  availability_zone      = var.subnet_az[0]
+  instance_type          = var.machine_type
+  key_name               = var.key_pair
+  vpc_security_group_ids = [aws_security_group.s3secgp.id]
+  //subnet_id = aws_subnet.s3subnets[0].id
+  root_block_device {
+    volume_size = 8
+  }
+  network_interface {
+    network_interface_id = aws_network_interface.s3netinf.id
+    device_index         = 1
+  }
 }
